671340828dcd2196c71c5710d43750049f371fa4
Pawan Kumar <pawank@exeire.com>
2024-12-02 14:31:44 +0530

    Add Employee

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,18 +8,33 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  styleUrl: './add-employee.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddEmployeeComponent implements OnInit {
   http = inject(HttpClient);
   userList: any[] = [];
-  isApi:Boolean = false;
+  isApi: Boolean = false;
+  name: string = 'Html';
+
+  constructor(private cdRef: ChangeDetectorRef) {
+
+  }
+
   ngOnInit(): void {
     this.isApi = true;
     this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((result: any) => {
       this.userList = result;
       this.isApi = false;
+      this.name = 'Java';
       console.log(this.userList);
+      setTimeout(() => {
+
+        this.cdRef.detectChanges();
+      }, 2000);
     })
+  }
+  reloadUi() {
+
   }
 }
